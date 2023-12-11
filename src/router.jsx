ec0721d8287fe89f03d5738bd77a275fed5a0590
jdesiderio <desiderio.janyce@gmail.com>
2023-12-11 14:50:20 +0100

    corr router

--- src/router.jsx
+++ src/router.jsx
@@ -1,24 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/home'
-import About from './pages/about'
-import CardInfo from './pages/cardInfo'
-import Header from './components/header'
-import Footer from './components/footer'
-import Error from './pages/error'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import About from './pages/about';
+import CardInfo from './pages/cardInfo';
+import Header from './components/header';
+import Footer from './components/footer';
+import Error from './pages/error';
 
 function AppRouter() {
   return (
-    <Router>
+    <Router basename="/kasa">
       <Header />
       <Routes>
-        <Route exact path="/kasa" element={<Home />} />
-        <Route path="/kasa/about" element={<About />} />
-        <Route path="/kasa/card/:id" element={<CardInfo />} />
-        <Route path="/kasa/*" element={<Error />} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/card/:id" element={<CardInfo />} />
+        <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
     </Router>
-  )
+  );
 }
 
-export default AppRouter
+export default AppRouter;
